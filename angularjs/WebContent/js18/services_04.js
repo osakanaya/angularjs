b5daa3d5baf4cec623d137beceb8d38dbaeff0e0
Hideo Sashida <osakanaya@gmail.com>
2015-01-07 16:34:54 +0900

    18章まで完了

---
+++ angularjs/WebContent/js18/services_04.js
@@ -0,0 +1,31 @@
+angular.module("customServices", [])
+.provider("logService", function() {
+	return {
+		messageCounterEnabled: function(setting) {
+			if (angular.isDefined(setting)) {
+				counter = setting;
+				return this;
+			} else {
+				return counter;
+			}
+		},
+		debugEnabled: function(setting) {
+			if (angular.isDefined(setting)) {
+				debug = setting;
+				return this;
+			} else {
+				return debug;
+			}
+		},
+		$get: function() {
+			return {
+				messageCount: 0,
+				log: function(msg) {
+					if (debug) {
+						console.log("(LOG" + (counter ? " + " + this.messageCount++ + ") " : ") ") + msg);
+					}
+				}
+			};
+		}
+	};
+});
